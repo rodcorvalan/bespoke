e088bc09d6e3c14a9a5149fd70d6ce2de0ce0332
Rodrigo Corvalán <rodcorvalan@live.com>
2019-02-27 10:24:18 -0300

    edit gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,35 @@
-'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const pleeease = require('gulp-pleeease');
 
-// Import && Config
 
-var gulp 		= require('gulp');
-var sass 		= require('gulp-sass');
-var sourcemaps 	= require('gulp-sourcemaps');
-var browserSync = require('browser-sync').create();
-var pleeease    = require('gulp-pleeease');
- 
-sass.compiler = require('node-sass');
+const armix_browsersync = () => {
+	browserSync.init({proxy: 'localhost/colegio',files: 'stylesheets/style.css'});
+}
 
-// Tasks
-
-gulp.task('browsersync',function() {
-	browserSync.init({proxy: 'localhost/arrocal',files: 'stylesheets/style.css'});
-});
-
-gulp.task('sass', function () {
+const armix_sass = () => {
 	return gulp.src('./sass/style.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass({
-			outputStyle: 'expanded',
-			includePaths: ['./node_modules/susy/sass']
-		}).on('error', sass.logError))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest('./stylesheets/'));
-});
- 
-gulp.task('watch', ['sass'], function () {
-	gulp.watch('sass/**/*.scss', ['sass']);
-});
+			   .pipe(sourcemaps.init())
+			   .pipe(sass().on('error', sass.logError))
+			   .pipe(sourcemaps.write('./'))
+			   .pipe(gulp.dest('./stylesheets/'));
+}
 
-gulp.task('prod', function () {
+const armix_watch = () => {
+	return gulp.watch('sass/**/*.scss', ['sass']);
+}
+
+const armix_prod = () => {
 	return gulp.src('sass/style.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(pleeease())
-		.pipe(gulp.dest('./stylesheets/'));
-});
+			   .pipe(sass().on('error', sass.logError))
+			   .pipe(pleeease())
+			   .pipe(gulp.dest('./stylesheets/'));
+	
+}
+
+exports.browsersync = armix_browsersync;
+exports.sass = armix_sass;
+exports.watch = armix_watch;
+exports.prod = armix_prod;
--- parts/organisms/header.php
+++ parts/organisms/header.php
@@ -2,6 +2,5 @@
 	<div class="container">
 		<?php include TD . '/parts/atoms/logo.php'; ?>
 		<?php include TD . '/parts/molecules/main-menu.php'; ?>
-		<?php include TD . '/parts/molecules/lang-menu.php'; ?>
 	</div>
 </header>
